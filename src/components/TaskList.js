463d67ad37fb5aa2a9a49d6f60fe503e23a67bff
Ori Roll <oriroll@gmail.com>
2020-05-01 10:27:00 +0300

    Api testing

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 import Task from "./Task.js";
 
+fetch("https://swapi.py4e.com/api/people/1")
+	.then(function (response) {
+		/* const dataPars = JSON.parse(data); */
+		response.json().then(function (data) {
+			console.log(data);
+		});
+	})
+	.catch(function (err) {
+		console.log("bug!", err);
+	});
+
 function TaskList() {
 	const [displayTasks, setDisplayTasks] = useState([{ name: "bla" }, { name: "blu" }]);
 
