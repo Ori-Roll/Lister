2ff74f54a1b5aec98805b8597715eaf8fc2da356
Ori Roll <oriroll@gmail.com>
2020-05-05 22:07:16 +0300

    Updates

---
+++ precache-manifest.2c51fc34da67169ba09bfba95f9f42bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95fcb556cc4d8971ec8830544a8ac200",
+    "url": "/SW-Lister/index.html"
+  },
+  {
+    "revision": "8f0f2c79ff0e8a178505",
+    "url": "/SW-Lister/static/css/main.c261369c.chunk.css"
+  },
+  {
+    "revision": "be244cef778d4303f547",
+    "url": "/SW-Lister/static/js/2.2aa8d495.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SW-Lister/static/js/2.2aa8d495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0f2c79ff0e8a178505",
+    "url": "/SW-Lister/static/js/main.e57bba7c.chunk.js"
+  },
+  {
+    "revision": "6fd015d1ec6a9039da43",
+    "url": "/SW-Lister/static/js/runtime-main.9ff314b8.js"
+  }
+]);
